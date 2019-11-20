466bf2caea78d3625ef61d3e1939dfd45b00df81
Dae-Hyun Lee @ XPS 9360 <daehyun.lee@gmail.com>
2019-11-20 09:23:09 +0900

    game world에 add_objects 추가.

--- Labs/Lecture15_AI/game_world.py
+++ Labs/Lecture15_AI/game_world.py
@@ -8,12 +8,18 @@ def add_object(o, layer):
     objects[layer].append(o)
 
 
+def add_objects(l, layer):
+    objects[layer] += l
+
+
 def remove_object(o):
     for i in range(len(objects)):
         if o in objects[i]:
             objects[i].remove(o)
             del o
             break
+
+
 
 
 def clear():
